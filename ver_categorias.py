b85ca600fefdf1daf10eed77b8a0b0beb91bbdbc
CarolinaE12 <Karo0198@outlook.com>
2026-04-01 18:39:14 -0500

    Datos

---
+++ ver_categorias.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset
+import pandas as pd
+
+archivo = "Electronics.jsonl.gz"
+try:
+    df = pd.read_json(archivo, lines=True, compression='gzip', nrows=5000)
+    print("¡Archivo cargado con éxito!")
+    print("\n Campos (Columnas) detectados ")
+    print(df.columns.tolist())
+    
+    print("\n Vista de datos ")
+    print(df[['rating', 'title', 'text']].head())
+
+except Exception as e:
+    print(f" Error: {e}")
+    print("El Archivo debe estar en la misma carpeta que el .py")
