88e6c099899681ac4aac1a8b5b771cb4185f586f
CarolinaE12 <Karo0198@outlook.com>
2026-04-07 21:21:59 -0500

    Inclusión de estilos CSS

--- app.py
+++ app.py
@@ -2,15 +2,14 @@ from flask import Flask, render_template
 import pandas as pd
 app = Flask(__name__)   
 
-def cargar_datos():
-    archivo = "data/Electronics.jsonl.gz"
-    df = pd.read_json(archivo, lines=True, compression='gzip', nrows=100)
-    return df[['rating', 'title', 'text']]
+#def cargar_datos():
+#   archivo = "data/Electronics.jsonl.gz"
+#   df = pd.read_json(archivo, lines=True, compression='gzip', nrows=100)
+#    return []
 
 @app.route("/")
 def index():
-    df = cargar_datos()
-    datos = df.to_dict(orient="records")
+    datos = []
     return render_template("index.html", datos=datos)
 
 if __name__ == "__main__":
